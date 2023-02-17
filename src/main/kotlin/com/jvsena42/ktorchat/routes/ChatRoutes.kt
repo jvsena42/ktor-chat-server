3478f11a0f0e80e72c5d46dbefaec15f86b3d7ae
jvsena42 <jvsena16@gmail.com>
2023-02-17 08:14:44 -0300

    feat: create chat socket route

---
+++ src/main/kotlin/com/jvsena42/ktorchat/routes/ChatRoutes.kt
@@ -0,0 +1,48 @@
+package com.jvsena42.ktorchat.routes
+
+import com.jvsena42.ktorchat.room.MemberAlreadyExistsException
+import com.jvsena42.ktorchat.room.RoomController
+import com.jvsena42.ktorchat.session.ChatSession
+import io.ktor.http.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.sessions.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.channels.consumeEach
+
+fun Route.chatSocket(roomController: RoomController) {
+    webSocket("/chat-socket") {
+        val session = call.sessions.get<ChatSession>()
+        if (session == null) {
+            close(CloseReason(CloseReason.Codes.VIOLATED_POLICY, "There is no session"))
+            return@webSocket
+        }
+
+        try {
+            roomController.onJoin(
+                userName = session.userName,
+                sessionId = session.sessionId,
+                session = this
+            )
+
+            incoming.consumeEach { frame ->
+                if (frame is Frame.Text) {
+                    roomController.sendMessage(
+                        senderUserName = session.userName,
+                        message = frame.readText()
+                    )
+                }
+            }
+
+        } catch (e: MemberAlreadyExistsException) {
+            call.respond(HttpStatusCode.Conflict)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            roomController.tryDisconnect(userName = session.userName)
+        }
+
+
+    }
+}
