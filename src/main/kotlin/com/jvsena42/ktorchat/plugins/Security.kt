9aca65c48c13e6eb9eabb362220aa466c5ad4492
jvsena42 <jvsena16@gmail.com>
2023-02-17 07:44:14 -0300

    feat: chat session class

--- src/main/kotlin/com/jvsena42/ktorchat/plugins/Security.kt
+++ src/main/kotlin/com/jvsena42/ktorchat/plugins/Security.kt
@@ -1,22 +1,11 @@
 package com.jvsena42.ktorchat.plugins
 
-import io.ktor.server.sessions.*
-import io.ktor.server.response.*
+import com.jvsena42.ktorchat.session.ChatSession
 import io.ktor.server.application.*
-import io.ktor.server.routing.*
+import io.ktor.server.sessions.*
 
 fun Application.configureSecurity() {
-    data class MySession(val count: Int = 0)
     install(Sessions) {
-        cookie<MySession>("MY_SESSION") {
-            cookie.extensions["SameSite"] = "lax"
-        }
-    }
-    routing {
-        get("/session/increment") {
-                val session = call.sessions.get<MySession>() ?: MySession()
-                call.sessions.set(session.copy(count = session.count + 1))
-                call.respondText("Counter is ${session.count}. Refresh to increment.")
-            }
+        cookie<ChatSession>("SESSION")
     }
 }
---
+++ src/main/kotlin/com/jvsena42/ktorchat/session/ChatSession.kt
@@ -0,0 +1,6 @@
+package com.jvsena42.ktorchat.session
+
+data class ChatSession(
+    val userName: String,
+    val sessionId: String
+)
