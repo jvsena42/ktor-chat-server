bd736437e35359328ddfcaffd8caa3a3e1c89f3b
jvsena42 <jvsena16@gmail.com>
2023-02-17 07:51:23 -0300

    feat: intercept session

--- src/main/kotlin/com/jvsena42/ktorchat/plugins/Security.kt
+++ src/main/kotlin/com/jvsena42/ktorchat/plugins/Security.kt
@@ -3,9 +3,18 @@ package com.jvsena42.ktorchat.plugins
 import com.jvsena42.ktorchat.session.ChatSession
 import io.ktor.server.application.*
 import io.ktor.server.sessions.*
+import io.ktor.util.*
 
 fun Application.configureSecurity() {
     install(Sessions) {
         cookie<ChatSession>("SESSION")
+    }
+
+    intercept(ApplicationCallPipeline.Plugins) {
+        if (call.sessions.get<ChatSession>() == null) {
+            //the parameter name shouldn't be userName instead of username?
+            val userName = call.parameters["username"] ?: "Guest"
+            call.sessions.set(ChatSession(userName, generateNonce()))
+        }
     }
 }
